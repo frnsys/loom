9d15a6ac3903055bc5368ed5ac08d32752d17abe
Francis Tseng <f+accounts@frnsys.com>
2017-10-04 18:25:53 -0400

    updated questions

--- sim/quiz/QUESTIONS.js
+++ sim/quiz/QUESTIONS.js
@@ -35,56 +35,91 @@ export default [{
   "func": function(ans) { return {'impulsiveness': 30 * ans, 'impatience': 20 * ans} }
 }, {
   "type": "likert",
-  "qid": "hunger_personality",
-  "question": "You embody a strict mind/body duality, and hunger rarely seems to have an effect on your emotional state.",
-  "func": function(ans) { return {'metabolism': 40 * (1 - ans) } }
-}, {
-  "type": "likert",
   "qid": "introvert_recharge",
   "question": "Usually, being alone makes you feel recharged rather than lonely.",
   "func": function(ans) { return {'sociability': 30 * (1 - ans) } }
 }, {
   "type": "likert",
-  "qid": "altered_states",
-  "question": "Altered states are desirable to you.",
-  "func": function(ans) { return {'metabolism': 10 * (1 - ans), 'tolerance': 40 * ans, 'impulsiveness': 10 * ans } }
+  "qid": "friend_priorities",
+  "question": "At a party, I would ignore my friends in order to talk to someone who would be interesting/advatangeous to my work and projects.",
+  "func": function(ans) { return {'impulsiveness': 20 * ans, 'sociability': 20 * ans, 'impatience': 50 * ans} }
+}, {
+  "type": "likert",
+  "qid": "handshakes",
+  "question": "The firmness of a handshake is a sign of character.",
+  "func": function(ans) { return {'impatience': 40 * ans, 'awkwardness': 10 * (1-ans) } }
+}, {
+  "type": "likert",
+  "qid": "how_you_doing",
+  "question": "When someone asks me how I'm doing, I talk about my work instead of my feelings.",
+  "func": function(ans) { return {'sociability': 20 * (1-ans), 'awkwardness': 10 * ans } }
 }, {
   "type": "likert",
   "qid": "self_discomfort",
   "question": "You enjoy talking about issues that make yourself uncomfortable.",
-  "func": function(ans) { return {'openness': 20 * ans, 'extraversion': 20 * ans, 'agreeableness': 10 * ans } }
+  "func": function(ans) { return {'impulsiveness': 20 * ans, 'sociability': 20 * ans, } }
 }, {
   "type": "multipleChoice",
-  "qid": "small_talk_topic",
-  "question": "What's your favorite small-talk topic?",
+  "qid": "forgot_name",
+  "question": "You see someone you know but forgot their name.",
   "answers": {
-    "The weather and global warming": ["#weather_convo#"],
-    "The latest in blockchain technology": ["#blockchain#", "#silicon_valley#"],
-    "Gentrification in Brooklyn": ["#real_estate#", "#race#"],
-    "A critique of Jacobin Magazine": ["#radical#", "#twitter#"]
+    "You carefully avoid them until they say hi": {
+      'impulsiveness': 50,
+      'awkwardness': 75
+    },
+    "Say hi but try not to mention their name in a conversation": {
+      'impulsiveness': 25,
+      'awkwardness': 25,
+      'sociability': 50
+    },
+    "Say 'Oh hi (mumble something indistinct)'": {
+      'awkwardness': 75,
+      'socialability': 25
+    },
+    "Fetch someone they don't know and force them to introduce ecah other": {
+      'impulsiveness': 25,
+      'sociability': 75,
+      'impatience': 75
+    },
+    "Pretend like you don't know them": {
+      'awkwardness': 100
+    },
+    "Just be straightforward: 'I'm sorry, but I forgot your name'": {
+      'impulsiveness': 25,
+      'social': 100
+    }
   },
+}, {
+  "type": "likert",
+  "qid": "altered_states",
+  "question": "D.A.R.E. inspired you to try drugs.",
+  "func": function(ans) { return {'metabolism': 10 * (1 - ans), 'tolerance': 40 * ans, 'impulsiveness': 10 * ans } }
+}, {
+  "type": "likert",
+  "qid": "hunger_personality",
+  "question": "You hangry?",
+  "func": function(ans) { return {'metabolism': 40 * ans } }
+}, {
+  "type": "likert",
+  "qid": "chips_butty",
+  "question": "A chips butty is a sandwich <img src='http://3.bp.blogspot.com/_ScBigNRg0Y4/S6vQ9acsYhI/AAAAAAAAAZY/PcWKnCpFq1E/s1600/chip-butty.jpg'>",
+  "func": function(ans) { return {'impatience': 30 * (1 - ans), 'metabolism':  25 * ans } }
 }, {
   "type": "checkboxes",
   "qid": "geek_topic",
   "question": "What topics would you want to geek-out about?",
   "answers": {
+    "Slash fan fiction": ["#fan_fic#"],
+    "The weather and global warming": ["#weather_convo#"],
+    "The latest Twitter hot-take": ["#twitter#", "#silicon_valley#"],
+    "Gentrification in Brooklyn": ["#real_estate#", "#race#"],
+    "A leftist critique of Democrats": ["#radical#", "#politics#"],
     "The latest and freshest Javascript library": ["#javascript_library#"],
     "The latest and freshest literary theory": ["#literary_theory#"],
     "The latest and freshest meme theory": ["#meme_theory#"],
     "The latest and freshest hole-in-the-wall restaurants": ["#restaurants#"],
-    "The latest and freshest CRISPR projects": ["#crispr#"],
     "The latest and freshest political gossip": ["#political_gossip#"],
-  },
-}, {
-  "type": "multipleChoice",
-  "qid": "favorite_test",
-  "question": "What's your favorite test?",
-  "answers": {
-    "The Bechdel test": ["#bechdel_test#"],
-    "The Voight-Kampff test": ["#voight_kampff_test#"],
-    "The Turing test": ["#turing_test"],
-    "The Myers-Briggs Test": ["#myers_briggs_test#"],
-    "The Purity Test": ["#purity_test#"]
+    "The Echo Chamber": ["#politics#", "#silicon_valley#"],
   },
 }, {
 	"type": "likert",
